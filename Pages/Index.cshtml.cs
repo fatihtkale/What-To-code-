f3388a968edc96b3c34ab55eb7dca19d0966002a
Faith <iosbroker2720@gmail.com>
2019-01-17 19:23:35 +0100

    Added files :+1:

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcoreapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [HttpPost]
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // Just here to refresh page :p
+            return null;
+        }
+    }
+}
---
+++ models/project.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace aspnetcoreapp.Pages
+{
+    public class projectsList
+    {
+        public static List<string> proj = new List<string>(){
+            "Discord Bot",
+            "AI that can get weather information",
+            "Learn a new programming language",
+            "Make a 2D shooter",
+            "Make a Aim bot",
+            "FTP Client",
+            "A random name generator",
+            "A chess engine",
+            "A movie showtime finder",
+            "Pixel art generator",
+            "Slack bot",
+            "Daily desktop background",
+            "Lunch picker",
+            "Date planner",
+            "‘Bring your umbrella’ notifier",
+            "HTTP Client",
+            "Build a simple operating system",
+            "A tax forecaster",
+            "A deal finder",
+            "An expense tracker",
+            "A bill splitter",
+            "Tax Calculator",
+            "Distance Between Two Cities",
+            "Calculator",
+            "Collatz Conjecture",
+            "Sorting - Implement two types of sorting algorithms: Merge sort and bubble sort.",
+            "Reverse a String"
+        };
+    }
+}
